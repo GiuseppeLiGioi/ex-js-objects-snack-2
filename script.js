43004b64049bf566ea9b6641f9329056554f5ba5
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-05-06 17:37:46 +0200

    added. snack 1

---
+++ script.js
@@ -0,0 +1,26 @@
+//🏆 Code Question 1
+
+
+/*
+const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // Double Cheese Burger
+console.log(secondBurger.name ); // Double Cheese Burger
+*/
+
+
+// Code Question 2
+const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?
+console.log(secondBurger.ingredients[0]); // ?
